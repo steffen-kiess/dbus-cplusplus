8dbbb9886c64ee7b2eab43ad128bca3f6cef0245
Steffen Kieß <Steffen.Kiess@ipvs.uni-stuttgart.de>
2019-10-07 15:54:52 +0200

    Fix writing to pipe (parameter must be a pointer to char, not the char itself)

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -83,5 +83,5 @@ ssize_t Pipe::read(void *buffer, unsigned int &nbytes)
 void Pipe::signal()
 {
   // TODO: ignoring return of read/write generates warning; maybe relevant for eventloop work...
-  ::write(_fd_write, '\0', 1);
+  ::write(_fd_write, "\0", 1);
 }
